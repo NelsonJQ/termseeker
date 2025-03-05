10fcf564ec358a01d94426a94f1cfd6cbe87d9bd
NelsonJQ <nelsonjaimesq@gmail.com>
2025-03-05 17:58:32 +0100

    updated setup with pckg dir as src

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="termun",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
         "python-docx",
         "pymupdf4llm",
